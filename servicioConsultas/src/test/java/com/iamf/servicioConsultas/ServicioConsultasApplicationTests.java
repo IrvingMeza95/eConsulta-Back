2ff2bdc63fb5ee428130d4f2d435ed6041ff7627
Irving Meza <irvingmeza95@gmail.com>
2025-01-21 16:39:36 -0600

    Se agrego el proyecto vacio de servicio consultas.

---
+++ servicioConsultas/src/test/java/com/iamf/servicioConsultas/ServicioConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamf.servicioConsultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
