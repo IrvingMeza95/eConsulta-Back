317d374008f0630f94a531eb5e154631e19e6d46
Irving Meza <irvingmeza95@gmail.com>
2025-01-21 19:06:46 -0600

    Se ajusto el proyecto de gestor de archivos.

---
+++ commons/src/main/java/com/iamf/commons/models/Paquete.java
@@ -0,0 +1,2 @@
+package com.iamf.commons.models;public class Paquete {
+}
---
+++ commons/src/main/java/com/iamf/commons/models/TipoServicio.java
@@ -0,0 +1,17 @@
+package com.iamf.commons.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity(name = "tipos_de_servicios")
+public class TipoServicio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 100)
+    private String nombre;
+}
